9392037e2d16c7b58050f8655accc625561775e4
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-06-10 23:34:37 +0200

    add loading and not-found pages

--- src/app/profiles/[id]/not-found.tsx
+++ src/app/profiles/[id]/not-found.tsx
@@ -1,33 +1,12 @@
 import Link from "next/link";
-import { Metadata } from 'next'
-import LikeButton from "@/app/components/like-button/LikeButton";
-import { notFound } from "next/navigation";
 
-
-type MetadataProps = {
-  params: { id: string }
-}
-
-export function generateMetadata({ params: { id } }: MetadataProps): Metadata {
-  return {
-    title: `Profile ${id}`,
-  }
-}
-
-
-export default function Profile({ params }: { params: { id: string } }) {
-
-  if (Number(params.id) === 7) {
-    return notFound();
-  }
-
+export default function NotFound() {
   return (
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
-          Profile {params.id}
+          Profile not found
         </p>
         <div className="flex items-center justify-between">
-          <LikeButton />
           <Link href='/profiles'>Go back to Profiles</Link>
         </div>
       </div>
